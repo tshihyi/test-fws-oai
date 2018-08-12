9e3370986d6a402dfe84d821f28a8a6654d2f1e4
shelly <tshihyi@gmail.com>
2018-08-12 15:07:19 +0800

    Refactor  duplicated  searchInsuredLog

--- TCB07-insured-log.js
+++ TCB07-insured-log.js
@@ -3,10 +3,17 @@ Feature('投保退保記錄')
 const login = require('./login.js')
 const farmers = require('./secret/profile-farmers.json')
 
-Before( I =>{
+Before(I => {
   login(I)
   I.amOnPage('/InsureLog/Index')
 })
+
+const searchInsuredLog = (I, id, name) => {
+  I.fillField('SearchByIDCard', id)
+  I.click('btnSearch')
+  I.waitForNavigation()
+  I.see(name)
+}
 
 Scenario('所有保險', I => {
   I.click('btnSearch')
@@ -15,21 +22,11 @@ Scenario('所有保險', I => {
   I.see('保險類型')
 
   I.selectOption('SearchByInsureType', '農保')
-  I.fillField('SearchByIDCard', farmers[0].id)
-  I.click('btnSearch')
-  I.waitForNavigation()
-  I.see(farmers[0].name)
+  searchInsuredLog(I, farmers[0].id, farmers[0].name)
 
   I.selectOption('SearchByInsureType', '職保')
-  I.fillField('SearchByIDCard', farmers[2].id)
-  I.click('btnSearch')
-  I.waitForNavigation()
-  I.waitForText(farmers[2].name)
+  searchInsuredLog(I, farmers[2].id, farmers[2].name)
 
   I.selectOption('SearchByInsureType', '健保')
-  I.fillField('SearchByIDCard', farmers[1].id)
-  I.click('btnSearch')
-  I.waitForNavigation()
-  I.see(farmers[1].name)
-
+  searchInsuredLog(I, farmers[1].id, farmers[1].name)
 })
