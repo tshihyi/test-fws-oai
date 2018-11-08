85bdfdc57ba21ad373d87e314ea572626370f814
tshihyi <tshihyi@gmail.com>
2018-11-08 17:40:14 +0800

    Add abnormal of submit for review

--- TCB03-09-oai-batch-insured.js
+++ TCB03-09-oai-batch-insured.js
@@ -1,7 +1,7 @@
 Feature('職保批次投保')
 
 const login = require('./login.js')
-const farmers = require('./secret/profile-farmers.json')
+const farmers = require('./secret/profile-oai-farmers.json')
 const id = farmers[0]["id"]
 
 Before(I => {
@@ -12,5 +12,11 @@ Before(I => {
 Scenario('職保批次投保-手動輸入身份證', I => {
     I.fillField('input#IDCard', id)
     I.click('button#InputBtn')
-    I.waitForText("職保已投保，請勿重複投保")
+    I.waitForText("重複匯入")
+})
+
+Scenario('職保批次投保-手動輸入身份證', I => {
+  I.fillField('input#IDCard', farmers[1]["id"])
+  I.click('button#InputBtn')
+  I.waitForText("送審中")
 })
