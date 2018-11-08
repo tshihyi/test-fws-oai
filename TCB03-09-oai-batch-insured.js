5235fd65c6ae3227b0b976eb35739e3ea9b1e25c
tshihyi <tshihyi@gmail.com>
2018-11-08 14:42:20 +0800

    Add keyin oai bath insured

--- TCB03-09-oai-batch-insured.js
+++ TCB03-09-oai-batch-insured.js
@@ -1,15 +1,16 @@
 Feature('職保批次投保')
 
 const login = require('./login.js')
+const farmers = require('./secret/profile-farmers.json')
+const id = farmers[0]["id"]
 
 Before(I => {
   login(I)
-  I.amOnPage('/Insure/OccupationalInjuryInsureBatchImportHistory')
+  I.amOnPage('Insure/OccupationalInjuryInsureManualInput')
 })
 
-Scenario('批次投保匯入瀏覽', I => {
-    I.see('新增匯入')
-    I.click('button.btn.default')
-    I.amOnPage('/Insure/OccupationalInjuryInsureBatchImport')
-    I.fillField('IDCard', 'H101636726')
+Scenario('職保批次投保-手動輸入身份證', I => {
+    I.fillField('input#IDCard', id)
+    I.click('button#InputBtn')
+    I.waitForText("職保已投保，請勿重複投保")
 })
